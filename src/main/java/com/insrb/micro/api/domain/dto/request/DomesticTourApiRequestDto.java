9ab50b6ba54a5d747360ac16764ec7203a375576
DESKTOP-SA0VI34\Seop <jongko54@gmail.com>
2023-11-08 16:43:38 +0900

    age,sex,period,gubun,startDate,endDate, beforePayment(결제 전 확인 칼럼) 추가

--- src/main/java/com/insrb/micro/api/domain/dto/request/DomesticTourApiRequestDto.java
+++ src/main/java/com/insrb/micro/api/domain/dto/request/DomesticTourApiRequestDto.java
@@ -17,10 +17,18 @@ public class DomesticTourApiRequestDto {
     private String juminBack;
     private String phoneNum;
     private String email;
+    private String age;
+    private String sex;
+    private String period;
+    private String gubun;
+    private Date startDate;
+    private Date endDate;
     private String diseasesThreeYearsAgreement;
     private String dangerLeisureSportsAgreement;
     private String travelPurpose;
     private String privacyInfoAgreement;
+    private String beforePayment;
+
     private String deleteYn;
 
     public DomesticTourApi toEntity() {
@@ -30,10 +38,17 @@ public class DomesticTourApiRequestDto {
           .juminBack(this.juminBack)
           .phoneNum(this.phoneNum)
           .email(this.email)
+          .age(this.age)
+          .sex(this.sex)
+          .period(this.period)
+          .gubun(this.gubun)
+          .startDate(this.startDate)
+          .endDate(this.endDate)
           .diseasesThreeYearsAgreement(this.diseasesThreeYearsAgreement)
           .dangerLeisureSportsAgreement(this.dangerLeisureSportsAgreement)
           .travelPurpose(this.travelPurpose)
           .privacyInfoAgreement(this.privacyInfoAgreement)
+          .beforePayment(this.beforePayment)
           .deleteYn('N')
           .build();
     }
--- src/main/java/com/insrb/micro/api/domain/entity/DomesticTourApi.java
+++ src/main/java/com/insrb/micro/api/domain/entity/DomesticTourApi.java
@@ -1,6 +1,7 @@
 package com.insrb.micro.api.domain.entity;
 
 import com.insrb.micro.admin.domain.entity.common.CommonEntity;
+import java.util.Date;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -26,26 +27,42 @@ public class DomesticTourApi extends CommonEntity {
   private String juminBack;
   private String phoneNum;
   private String email;
+  private String age;
+  private String sex;
+  private String period;
+  private String gubun;
+  private Date startDate;
+  private Date endDate;
   private String diseasesThreeYearsAgreement;//최근 3년 내에 특정질병
   private String dangerLeisureSportsAgreement;//위험한 레포츠
   private String travelPurpose;//여행 목적
   private String privacyInfoAgreement;//개인정보수집 동의
+  private String beforePayment;//결제 전
+
 
   @Column(name = "deleteYn")
   private char deleteYn;
 
   @Builder
-  public DomesticTourApi(long id,String userName,String juminFront,String juminBack,String phoneNum,String email,
-      String diseasesThreeYearsAgreement,String dangerLeisureSportsAgreement,String travelPurpose,String privacyInfoAgreement, char deleteYn) {
+  public DomesticTourApi(long id,String userName,String juminFront,String juminBack,String phoneNum,String email,String age,
+      String sex,String period,String gubun, Date startDate,Date endDate,
+      String diseasesThreeYearsAgreement,String dangerLeisureSportsAgreement,String travelPurpose,String privacyInfoAgreement, String beforePayment,char deleteYn) {
     this.userName = userName;
     this.juminFront = juminFront;
     this.juminBack   = juminBack;
     this.phoneNum = phoneNum;
     this.email = email;
+    this.age = age;
+    this.sex = sex;
+    this.period = period;
+    this.gubun = gubun;
+    this.startDate = startDate;
+    this.endDate = endDate;
     this.diseasesThreeYearsAgreement = diseasesThreeYearsAgreement;
     this.dangerLeisureSportsAgreement = dangerLeisureSportsAgreement;
     this.travelPurpose = travelPurpose;
     this.privacyInfoAgreement = privacyInfoAgreement;
+    this.beforePayment = beforePayment;
 
     this.deleteYn = deleteYn;
   }
