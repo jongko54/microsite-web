7d41f774f2d52902556dd889694d2d38ff11cc2a
minseo <kms@bankq.co.kr>
2023-11-13 20:21:18 +0900

    조건식 수정

--- src/main/java/com/insrb/micro/api/service/MydataInsuranceApiService.java
+++ src/main/java/com/insrb/micro/api/service/MydataInsuranceApiService.java
@@ -9,11 +9,13 @@ import com.insrb.micro.api.domain.entity.MydataInsuranceApi;
 import com.insrb.micro.api.exception.CustomException;
 import com.insrb.micro.api.exception.ErrorCode;
 import com.insrb.micro.api.repository.MydataInsuranceApiRepository;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 import javax.transaction.Transactional;
+
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.PageRequest;
@@ -24,46 +26,49 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class MydataInsuranceApiService {
-  private final MydataInsuranceApiRepository mydataInsuranceApiRepository;
-  private final MydataUserRepository mydataUserRepository;
+    private final MydataInsuranceApiRepository mydataInsuranceApiRepository;
+    private final MydataUserRepository mydataUserRepository;
 
-  @Transactional
-  public List<MydataInsuranceApiResponseDto> mydataInsuranceList(String id, Long userId){
-    List<MydataInsuranceApi> list = new ArrayList<>();
+    @Transactional
+    public List<MydataInsuranceApiResponseDto> mydataInsuranceList(String id, Long userId) {
+        List<MydataInsuranceApi> list = new ArrayList<>();
 
-    if(id == null){
-      list = mydataInsuranceApiRepository.findAll(Sort.by(Sort.Direction.DESC, "id"));
-    }else{
-      list = mydataInsuranceApiRepository.findAllById(id, Sort.by(Sort.Direction.DESC, "id"));
+        if (id == null) {
+            list = mydataInsuranceApiRepository.findAll(Sort.by(Sort.Direction.DESC, "id"));
+        } else {
+            list = mydataInsuranceApiRepository.findAllById(id, Sort.by(Sort.Direction.DESC, "id"));
+        }
+
+        Optional<MydataUser> findUser = mydataUserRepository.findById(userId);
+        if (findUser.isEmpty())
+            return list.stream().map(MydataInsuranceApiResponseDto::new).collect(Collectors.toList());
+
+        return list.stream()
+                .filter(insurance -> insurance.getKeyword().contains(findUser.get().getHousingType())
+                        || insurance.getKeyword().contains(findUser.get().getHousingDivision())
+                        || getDisease(findUser.get(), insurance))
+                .map(MydataInsuranceApiResponseDto::new)
+                .collect(Collectors.toList());
     }
 
-    Optional<MydataUser> findUser = mydataUserRepository.findById(userId);
-    if (findUser.isEmpty())
-      return list.stream().map(MydataInsuranceApiResponseDto::new).collect(Collectors.toList());
+    private boolean getDisease(MydataUser findUser, MydataInsuranceApi insurance) {
+        if (findUser.getDisease() == null) {
+            return true;
+        }
 
-    return list.stream()
-        .filter(insurance -> insurance.getKeyword().contains(findUser.get().getHousingType())
-            || insurance.getKeyword().contains(findUser.get().getHousingDivision())
-            || motorCycle(findUser.get(), insurance))
-        .map(MydataInsuranceApiResponseDto::new)
-        .collect(Collectors.toList());
-  }
+        if (!"".equals(findUser.getDisease())) {
+            return insurance.getKeyword().contains(findUser.getDisease());
+        }
+        return true;
+    }
 
-  private boolean motorCycle (MydataUser findUser, MydataInsuranceApi insurance) {
-      if (!"".equals(findUser.getMotorcycle())) {
-        return insurance.getKeyword().contains(findUser.getMotorcycle());
-      }
-      return true;
-  }
-
-  public MydataInsuranceApiResponseDto mydataInsuranceDetail(long param) {
-    MydataInsuranceApi entity = mydataInsuranceApiRepository.findById(param).orElseThrow(() -> new CustomException(
-        ErrorCode.POSTS_NOT_FOUND));
+    public MydataInsuranceApiResponseDto mydataInsuranceDetail(long param) {
+        MydataInsuranceApi entity = mydataInsuranceApiRepository.findById(param).orElseThrow(() -> new CustomException(
+                ErrorCode.POSTS_NOT_FOUND));
 
 
-    return new MydataInsuranceApiResponseDto(entity);
-  }
-
+        return new MydataInsuranceApiResponseDto(entity);
+    }
 
 
 }
